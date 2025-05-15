8289f45b65787596222e3a152f7f8c65abbe9a89
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2025-05-15 21:12:17 +0200

    neogit trigger

--- nvim/lua/slava/remap.lua
+++ nvim/lua/slava/remap.lua
@@ -50,5 +50,8 @@ vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) 
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+-- open neogit
+vim.keymap.set("n", "<leader>g", "<cmd>Neogit kind=vsplit<CR>")
+
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/dotfiles/mf<CR>")
