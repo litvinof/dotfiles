1bd17fef2da4825769b9b738cd254cc543e7e8db
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2025-08-26 12:48:21 +0200

    typo

--- nvim/after/plugin/buffers.lua
+++ nvim/after/plugin/buffers.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>h", "<cmd>Telescope buffers sort_mru=true sort_lastused=true", { desc = "Open telecope buffers" })
+vim.keymap.set("n", "<leader>h", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>", { desc = "Open telecope buffers" })
