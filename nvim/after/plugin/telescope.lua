863d95c8119f3f6c6002aff56f0e347036e28ecb
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2025-08-26 13:10:39 +0200

    minor

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,10 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Search > ") });
+    builtin.grep_string({ search = vim.fn.input("Search ❭ ") });
 end)
 
 
@@ -17,6 +19,6 @@ require('telescope').setup({
             "node_modules",
             ".git"
         },
-        initial_mode = "normal"
+        initial_mode = "insert"
     }
 })
