4dfd8c50a7fa21ffaa87c3cec607a5e32312d224
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2025-10-29 15:43:11 +0100

    global search visual

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -7,6 +7,30 @@ vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Search ❭ ") });
 end)
 
+vim.keymap.set('v', '<leader>pS', function()
+    -- Save current register contents
+    local saved_reg = vim.fn.getreg('"')
+    local saved_regtype = vim.fn.getregtype('"')
+
+    -- Yank visual selection into unnamed register
+    vim.cmd('normal! ""y')
+    local text = vim.fn.getreg('"')
+
+    -- Restore old register
+    vim.fn.setreg('"', saved_reg, saved_regtype)
+
+    -- Trim and sanitize selection
+    text = text:gsub("\n", " "):gsub("^%s+", ""):gsub("%s+$", "")
+
+    if text == nil or text == '' then
+        print("No visual selection")
+        return
+    end
+
+    -- Launch Telescope grep_string with the selected text
+    builtin.grep_string({ search = text })
+end, { desc = "Global search for selected text" })
+
 
 require('telescope').setup({
     pickers = {
