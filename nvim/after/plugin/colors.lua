9a1ac3ea65937e0c6dbc43c3c6feae91fe6e4e48
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2025-12-08 15:47:35 +0100

    colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,13 +1,35 @@
 function ColorItBaby(color)
-	-- color = color or "iceberg"
-	color = color or "melange"
-	-- color = color or "gruber-darker"
-	vim.cmd.colorscheme(color)
+    color = color or "melange"
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- Enable italics in melange theme
+    vim.g.melange_enable_font_variants = {
+        bold = true,
+        italic = true, -- Enable italics globally
+        underline = true,
+        undercurl = true,
+        strikethrough = true
+    }
 
-	vim.api.nvim_set_hl(0, "Visual", { bg = "#727272" })
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#727272" })
+
+    -- Disable italics for everything except Comment
+    local no_italic_groups = {
+        "String",
+        "@string.special.symbol",
+        "@lsp.typemod.variable.globalScope",
+        "@markup.italic",
+    }
+
+    for _, group in ipairs(no_italic_groups) do
+        local hl = vim.api.nvim_get_hl(0, { name = group })
+        if next(hl) ~= nil then -- Check if highlight group exists
+            hl.italic = false
+            vim.api.nvim_set_hl(0, group, hl)
+        end
+    end
 end
 
 ColorItBaby()
